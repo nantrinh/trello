e92c3980d5754dcbb406e2776c0ea1d120586c4e
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-02-25 10:08:36 -0800

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Board.destroy_all
+List.destroy_all
+Card.destroy_all
+
+board1 = Board.create title: "First board"
+
+list1 = List.create title: "first list", board: board1
+list2 = List.create title: "second list", board: board1
+
+card1 = Card.create title: "card 1 list 1", list: list1
+card2 = Card.create title: "card 2 list 1", list: list1
+
+card1 = Card.create title: "card 1 list 2", list: list2
+card2 = Card.create title: "card 2 list 2", list: list2
+card3 = Card.create title: "card 3 list 2", list: list2
