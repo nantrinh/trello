1557b841612999184b1f867197d31ecb49d87696
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-02-25 11:15:57 -0800

    Add comments from Srdjan

--- app/javascript/reducers/BoardsReducer.js
+++ app/javascript/reducers/BoardsReducer.js
@@ -1,12 +1,12 @@
 export default function boardsReducer(state = [], action) {
-  if (action.type === 'FETCH_BOARDS_SUCCESS') {
+  if (action.type === "FETCH_BOARDS_SUCCESS") {
     return action.boards;
-  } else if (action.type === 'CREATE_BOARD_SUCCESS') {
+  } else if (action.type === "CREATE_BOARD_SUCCESS") {
     const newBoard = action.board;
     newBoard.id = Number(newBoard.id);
 
     return state.concat(newBoard);
-  } else if (action.type === 'FETCH_BOARD_SUCCESS') {
+  } else if (action.type === "FETCH_BOARD_SUCCESS") {
     const excludedBoards = state.filter(board => board.id !== action.board.id);
     const { lists, ...newBoardWithoutLists } = action.board;
 
@@ -15,3 +15,8 @@ export default function boardsReducer(state = [], action) {
     return state;
   }
 }
+
+// fetchBoardSuccess returns only boards, without lists
+// create listsReducer that handles FETCH_BOARD_SUCCESS, returns only lists
+// create cardsReducer  that handles FETCH_BOARD_SUCCESS, returns only cards
+// add state for lists and cards
