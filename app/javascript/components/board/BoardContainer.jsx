15733a192e80d4d8548d2b02edc8806cfdbbfc12
DavidPerich <davidnperich@gmail.com>
2020-02-27 04:59:42 +1100

    Setting  state and dispatch as props

--- app/javascript/components/board/BoardContainer.jsx
+++ app/javascript/components/board/BoardContainer.jsx
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
-import React from "react";
 import Board from "./Board";
+import * as actions from "../../actions/BoardActions";
 
 const mapStateToProps = (state, ownProps) => {
   const id = Number(ownProps.match.params.id);
@@ -10,10 +10,11 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const id = Number(ownProps.match.params.id);
   return {
     onFetchBoard: () => {
-      store.dispatch(actions.fetchBoard()).catch(error => console.log(error)); // do we need this?
+      store.dispatch(actions.fetchBoard(id)).catch(error => console.log(error)); // do we need this?
     }
   };
 };
