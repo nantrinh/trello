9bd807e1219c4fbe25e415d6431cd4b390b16967
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-02-28 08:43:02 -0800

    Got rid of params_merge

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -4,7 +4,7 @@ class Api::ListsController < ApplicationController
 
   def create
     board = Board.find(params[:board_id])
-    @list = List.new(list_params.merge(board: board))
+    @list = List.new(list_params)
     @list.board_id = board.id
 
     if @list.save
