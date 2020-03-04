036f2701a8ec357fac89db68380e7c20f2e175e4
Melissa Manousos <mmanousos@gmail.com>
2020-03-04 09:45:28 -0800

    Remove marked out code in CardActions

--- app/javascript/actions/CardActions.js
+++ app/javascript/actions/CardActions.js
@@ -24,26 +24,3 @@ export function createCard(list_id, title, callback) {
     });
   };
 }
-
-// export function updateListSuccess(list) {
-//   return { type: types.UPDATE_LIST_SUCCESS, list: list };
-// }
-
-// export function updateList(listId, title, callback) {
-//   const payload = {
-//     list: {
-//       title
-//     }
-//   };
-
-//   return function(dispatch) {
-//     // NOTE: newList => {...} is a callback
-//     apiClient.updateList(listId, payload, updatedList => {
-//       dispatch(updateListSuccess(updatedList));
-
-//       if (callback) {
-//         callback();
-//       }
-//     });
-//   };
-// }
