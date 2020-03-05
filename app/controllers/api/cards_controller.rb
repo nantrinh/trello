0cbba210acd857260b45d39e445adf15a05add2f
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-03-05 09:43:54 -0800

    Allow user to update card through API
    NOTE: Cards can now be created with additional attributes.
    
    For example, the following command will create a card with labels.
    http POST localhost:3000/api/cards <<< '{"list_id": 133, "card": {"title": "testing multiple attributes", "labels":["red", "blue"]}}'
    
    According to 1.11.4 of the API docs, this is valid behavior: "The only required field is the title." I interpreted this to mean that title is required, others are optional, but also acceptable.

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::CardsController < ApplicationController
   def create
     list = List.find(params[:list_id])
@@ -11,23 +13,46 @@ class Api::CardsController < ApplicationController
       render 'api/shared/error', status: :unprocessable_entity
     end
   rescue ActiveRecord::RecordNotFound
-    @error = "Invalid list data provided"
+    @error = 'Invalid list data provided'
     render 'api/shared/error', status: 404
   rescue ActionController::ParameterMissing
-    @error = "Invalid card data provided"
-    render 'api/shared/error', status: :unprocessable_entity  
+    @error = 'Invalid card data provided'
+    render 'api/shared/error', status: :unprocessable_entity
   end
 
   def show
     @card = Card.find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    @error = "Invalid card data provided"
+    @error = 'Invalid card data provided'
     render 'api/shared/error', status: 404
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    if @card.update(card_params)
+      render :update, status: 200
+    else
+      @error = @card.errors.full_messages.join(', ')
+      render 'api/shared/error', status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    @error = 'Invalid card id provided'
+    render 'api/shared/error', status: 404
+  rescue ActionController::ParameterMissing
+    @error = 'Invalid card data provided'
+    render 'api/shared/error', status: :unprocessable_entity
   end
 
   private
 
   def card_params
-    params.require(:card).permit(:title)
+    params.require(:card)
+          .permit(:title,
+                  :list_id,
+                  :description,
+                  :archived,
+                  :due_date,
+                  :completed,
+                  labels: [])
   end
 end
