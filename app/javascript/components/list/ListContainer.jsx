18ee73b0e198764e34beaef34a30cfe5b9aed8c1
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-03-05 08:50:47 -0800

    Add import statement

--- app/javascript/components/list/ListContainer.jsx
+++ app/javascript/components/list/ListContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import List from "./List";
+import * as actions from "../../actions/CardActions";
 
 const mapStateToProps = state => {
   return {
