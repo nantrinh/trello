9e446bf4b360c2d5c0b0cf88173b5beda6c81a79
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-03-06 10:48:57 -0800

    Reorder cards in UI (not persistent)

--- app/javascript/components/card/CardsContainer.jsx
+++ app/javascript/components/card/CardsContainer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { connect } from "react-redux";
+import dragula from "react-dragula";
 import Card from "./Card";
 
 const mapStateToProps = state => {
@@ -9,6 +11,10 @@ const mapStateToProps = state => {
 };
 
 class CardsContainer extends React.Component {
+  componentDidMount() {
+    var container = ReactDOM.findDOMNode(this);
+    dragula([container]);
+  }
   render() {
     const cards = this.props.cards
       .filter(card => card.list_id === Number(this.props.listId))
